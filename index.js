0e310d71152b5d99aebba50754afd36ce14c0da6
chapel_k <kevin@wecandoo.fr>
2020-05-23 11:26:56 +0200

    done

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var express = require('express'),
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/hybride');
+mongoose.connect(process.env.MONGODB_URI);
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
