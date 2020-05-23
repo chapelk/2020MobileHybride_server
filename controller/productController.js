d1ee37ce08fcb47bf0005c4c229258650f329a6d
chapel_k <kevin@wecandoo.fr>
2020-05-23 11:34:25 +0200

    done

--- controller/productController.js
+++ controller/productController.js
@@ -12,12 +12,30 @@ exports.get = function(req, res) {
     const {range, lat, long, category} = req.body;
     Product.find().lean().exec(function (err, products) {
         products.map(product => {
-            product.distance = 50; // TODO METTRE BON CALCUL
+            product.distance = getDistanceFromLatLonInKm(lat, long, product.lat, product.long);
             return product;
         });
         return res.status(200).send(products);
     });
 };
+
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lon2-lon1);
+    var a =
+        Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+        Math.sin(dLon/2) * Math.sin(dLon/2)
+    ;
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c; // Distance in km
+    return d;
+}
+
+function deg2rad(deg) {
+    return deg * (Math.PI/180)
+}
 
 exports.add =  function(req, res) {
     const {name, price, lat, long, description, category, lieu} = req.body;
